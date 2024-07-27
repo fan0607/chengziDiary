a61a7cff8e561632c0b0adaf59706dfe644053ea
fan_c <2207771247@qq.com>
2024-07-27 12:17:42 +0800

    three+shader背景

---
+++ docs/.vitepress/theme/HomeLayout.vue
@@ -0,0 +1,36 @@
+<script setup>
+import BlogTheme from '@sugarat/theme'
+import ThreeScene from './ThreeScene.vue'
+
+const { Layout } = BlogTheme
+</script>
+
+<template>
+  <Layout>
+    <template #home-hero-before>
+      <div id="custom-content">
+        <ThreeScene />
+      </div>
+    </template>
+  </Layout>
+</template>
+
+<style scoped>
+#custom-content {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: -1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+#custom-content h1 {
+  color: white;
+  margin-bottom: 20px;
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,21 @@
-import BlogTheme from '@sugarat/theme'
+// import BlogTheme from '@sugarat/theme'
 
-// 自定义样式重载
+// // 自定义样式重载
+// import './style.scss'
+
+// // 自定义主题色
+// // import './user-theme.css'
+
+// export default BlogTheme
+
+// .vitepress/theme/index.js
+import BlogTheme from '@sugarat/theme'
+import MyLayout from './HomeLayout.vue'
+
 import './style.scss'
 
-// 自定义主题色
-// import './user-theme.css'
-
-export default BlogTheme
+export default {
+  extends: BlogTheme,
+  // 使用注入插槽的包装组件覆盖 Layout
+  Layout: MyLayout
+}
